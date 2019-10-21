ceb742f29a82e39db70fee25c5d107bb80a3da50
Nick Spain <nicholas.spain96@gmail.com>
2019-10-22 00:23:07 +1100

    Use camel case for JSON

--- api/schemas.py
+++ api/schemas.py
@@ -4,16 +4,16 @@ from marshmallow import Schema, fields, ValidationError
 class UserRequestSchema(Schema):
     """Schema for a create user request which we use marshmallow to validate."""
 
-    first_name = fields.Str()
-    last_name = fields.Str()
+    first_name = fields.Str(data_key="firstName")
+    last_name = fields.Str(data_key="lastName")
     email = fields.Email()
     password = fields.Str()
 
 
 class UserResponseSchema(Schema):
     id = fields.Integer()
-    first_name = fields.Str()
-    last_name = fields.Str()
+    first_name = fields.Str(data_key="firstName")
+    last_name = fields.Str(data_key="lastName")
     email = fields.Email()
     roles = fields.List(fields.String())
 
