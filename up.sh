5ff43d44f3fff5e14b79fb0c0221738c64339366
Nick Spain <nicholas.spain96@gmail.com>
2019-10-24 20:23:52 +1100

    Add script for starting frontend and backend in docker containers

--- up.sh
+++ up.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker-compose -f api/docker-compose.yml -f frontend/docker-compose.yml up $@
+echo "=> Building backend image..."
+docker build -t nick96/attendance-badgetracking-backend -f api/Dockerfile api
+echo "=> Building frontend image..."
+docker build -t nick96/attendance-badgetracking-frontend -f frontend/Dockerfile frontend
+echo "=> Starting..."
+source api/.env
+env $(cat api/.env) $(cat frontend/.env) docker-compose -f api/docker-compose.yml -f frontend/docker-compose.yml up "$@"
